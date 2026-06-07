a27fd0456c485c389e0c4d0701ea6bb6fb3112ad
MahiSrk <maheshshirke1921@gmail.com>
2026-06-08 00:06:19 +0530

    add search occurences in array

--- src/com/dsa/array/A02_SearchAllOccurrences.java
+++ src/com/dsa/array/A02_SearchAllOccurrences.java
@@ -27,6 +27,23 @@ public class A02_SearchAllOccurrences {
 		return output;	
 	}
 	
+	public static int[] searchAll2(int[] arr,int element) {
+		
+		int n=arr.length;
+		int[] output = new int[n+1];
+		int j =0;
+		
+		for (int i=0; i<n; i++) {
+			if(arr[i]==element) {
+				output[j]=i;
+				j++;
+			}
+		}
+		//terminating output by -1
+		output[j]=-1;
+		return output;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
@@ -34,7 +51,14 @@ public class A02_SearchAllOccurrences {
 		int [] output = searchAll(arr,7);
 		System.out.println(Arrays.toString(arr));
 		System.out.println(Arrays.toString(output));
-	
+		
+		System.out.println("------------------------------");
+		
+		int [] output1 = searchAll2(arr,5);
+		System.out.println(Arrays.toString(arr));
+		for(int j=0; output1[j]!=-1;j++) {
+			System.out.print(output1[j]+ " ");
+		}
 	}
 
 }
